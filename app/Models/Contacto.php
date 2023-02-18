8cacf65a936e9c87b3307b2c1652fb435c135914
Leonardo Pinto <pintoleonardo1023@gmail.com>
2023-02-17 22:26:53 -0500

    crud

---
+++ app/Models/Contacto.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Contacto
+ *
+ * @property $id
+ * @property $direccion
+ * @property $telefono
+ * @property $correo
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Contacto extends Model
+{
+    
+    static $rules = [
+		'direccion' => 'required',
+		'telefono' => 'required',
+		'correo' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['direccion','telefono','correo'];
+
+
+
+}
